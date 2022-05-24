054a4ac4b04573e897eb3e4d61fa8b5969bf17c6
0line <horuzer@gmail.com>
2022-05-24 11:52:56 -0500

    cambiar configuracion post

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -13,16 +13,11 @@ export async function get(endpoint:string){
 export async function post(endpoint:string, data:any){
     return await fetch("https://mealsfoodapi-0line.vercel.app"+endpoint,{
         method: 'POST',
-        mode: 'cors', // no-cors, *cors, same-origin,
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin',
         headers: {
             'Content-Type': 'application/json',
-            'Origin': 'https://alegra.onrender.com'
+            'Origin': '*'
             // 'Content-Type': 'application/x-www-form-urlencoded',
         },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *client
         body: data
      })
     .then(function (resultado){
